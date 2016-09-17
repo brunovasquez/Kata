a947e7b37d5d43c9f52fda562cb00d2dfad83e91
brunovasquez <Bruno.Vasquez@fundacion-jala.org>
2016-09-17 00:06:26 -0400

    Ready to Code Review

--- src/main/java/org/fundacionjala/cata/ValidatorEAN.java
+++ src/main/java/org/fundacionjala/cata/ValidatorEAN.java
@@ -1,31 +1,67 @@
 package org.fundacionjala.cata;
 
+import java.util.stream.IntStream;
+
+
 /**
- * Created by BrunoVasquez on 8/26/2016.
+ * Handles the calculates of area and perimeter.
+ *
+ * @autor Bruno Vasquez
  */
 public class ValidatorEAN {
+    private static final int LAST_DIGIT = 12;
+    private static final int FACTOR_TEN = 10;
+    private static final int ZERO = 0;
+    private static final int FACTOR_THREE = 3;
+    private static final int FACTOR_TWO = 2;
+    private int totalSum;
+    private String code;
 
-
-    public boolean validate(String code) {
-        int valour = 0;
-        int sum = 0;
-        for (int i = 0; i < code.length(); i++) {
-            int number = code.charAt(i) - '0';
-
-            if (i == i % 2)
-                valour = number * 1;
-            else
-                valour = number * 0;
-
-            sum += valour;
-        }
-
-
-        if (code.length() - 1 == 10 - (sum % 10))
-            return true;
-
-        return false;
+    /**
+     * It is the constructor of the validator.
+     *
+     * @param code an string to valid.
+     */
+    public ValidatorEAN(String code) {
+        this.code = code;
+        totalSum = 0;
+        IntStream.range(0, this.code.length() - 1).forEach(index -> sum(index));
     }
 
+    /**
+     * Validates of code
+     *
+     * @return a boolean if the code is valid.
+     */
+    public boolean verifyChecksumWithLastDigit() {
+        return checksum() == charToDigit(LAST_DIGIT);
+    }
 
+    /**
+     * Checks the total sum of the digit
+     *
+     * @return the checksum.
+     */
+    public int checksum() {
+        return (totalSum % FACTOR_TEN == ZERO) ? ZERO : FACTOR_TEN - (totalSum % FACTOR_TEN);
+    }
+
+    /**
+     * Sums the digits according his position par or impair
+     *
+     * @param index a integer with the position of the digit
+     */
+    public void sum(int index) {
+        totalSum += (index % FACTOR_TWO == ZERO) ? charToDigit(index) : charToDigit(index) * FACTOR_THREE;
+    }
+
+    /**
+     * Casts the char digit to integer digit
+     *
+     * @param index a integer with the position of the digit
+     * @return a integer digit.
+     */
+    public int charToDigit(int index) {
+        return code.charAt(index) - '0';
+    }
 }
--- src/test/java/org/fundacionjala/cata/ValidatorEANTest.java
+++ src/test/java/org/fundacionjala/cata/ValidatorEANTest.java
@@ -1,32 +1,32 @@
 package org.fundacionjala.cata;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Test for {@link ValidatorEAN}
+ */
 public class ValidatorEANTest {
 
-    private ValidatorEAN validator;
-    @Before
-    public void setUp() {
-        validator = new ValidatorEAN();
-    }
     @Test
     public void test_validate_true() {
         final String code = "9783815820865";
-        assertTrue(validator.validate(code));
+        ValidatorEAN validator = new ValidatorEAN(code);
+        assertTrue(validator.verifyChecksumWithLastDigit());
     }
 
     @Test
     public void test_validate_false() {
         final String code = "9783815820864";
-        assertFalse(validator.validate(code));
+        ValidatorEAN validator = new ValidatorEAN(code);
+        assertFalse(validator.verifyChecksumWithLastDigit());
     }
 
     @Test
     public void test_validate_otherTrue() {
         final String code = "9783827317100";
-        assertTrue(validator.validate(code));
+        ValidatorEAN validator = new ValidatorEAN(code);
+        assertTrue(validator.verifyChecksumWithLastDigit());
     }
 }
